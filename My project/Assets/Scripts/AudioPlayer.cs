b3a708ab2a7278e3860887f9a74faac1146f0837
wizLucky <nagadon0424@icloud.com>
2023-01-09 04:43:59 +0900

    add OpenJTalk

---
+++ My project/Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AudioPlayer : MonoBehaviour
+{
+    private void Start()
+    {
+        // OpenJTalk.Speak("こんにちは、良い天気ですね");
+    }
+}
